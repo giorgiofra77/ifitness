a03a5f570c0fed3751b4844f878c447dd89b6ef4
Giorgio <36672870+giorgiofra77@users.noreply.github.com>
2023-01-29 17:29:16 +0100

    Delete storage directory

--- storage/framework/views/b18a783347e20286fa5c1031b3439241f1595fb1.php
+++
@@ -1,6 +0,0 @@
-<div class="list-group mt-1 col-auto">
-    <a href="<?php echo e(route('customers.show', $customer->id)); ?>" title="Scheda atleta" class="list-group-item list-group-item-action py-2 fs-5">
-        <?php echo e($customer->fullname); ?><i class="bi bi-chevron-right position-absolute end-0 me-2"></i></a>
-</div>
-
-<?php /**PATH /Users/giorgio/sviluppo/laravel/iFitness/resources/views/customers/listgroup.blade.php ENDPATH**/ ?>
